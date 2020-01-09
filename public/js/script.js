c08f3f65bff506a12e9228c4d910262fed07da09
Arun <arunmadcoder@gmail.com>
2020-01-09 09:26:15 +0530

    Mobile fixes

--- public/js/script.js
+++ public/js/script.js
@@ -9,5 +9,16 @@ $(window).scroll(function (event) {
     if(current <= 315){
       ele1.css({marginLeft:(current-315)+'px'});
     }
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if(isMobile){
+      current = current - 1100;
+    }
     ele2.css({marginLeft: total1 - current +'px'});
 });
+
+// $(document).ready(function(){
+//   var self = this;
+//     setTimeout(function(){
+//       $(self).scrollTop(0);
+//     },0)
+// });
