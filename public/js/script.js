0757984b90f3b8bd25f99846f33ad6ead42cfaf1
Arun <arunmadcoder@gmail.com>
2020-01-12 14:07:14 +0530

    Fireworks animation

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,7 @@ var ele1 = $(".caricature");
 var currPosition1 = ele1.position().left + 100;
 var ele2 = $(".train");
 var total1 = $(window).width();
+var showFireworks = 500;
 $(window).scroll(function (event) {
     current = $(window).scrollTop();
     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
@@ -11,6 +12,15 @@ $(window).scroll(function (event) {
       ele1.css({marginLeft:(current-315)+'px'});
       
     }
+    if(isMobile){
+      showFireworks = 900;
+    }
+    if(current >= showFireworks){
+      $(".caricature-block .content-div .invitation-content .name").addClass('fireworks');
+    }else{
+      $(".caricature-block .content-div .invitation-content .name").removeClass('fireworks');
+    }
+
     if(isMobile){
         var scale = current/350 > 1 ? 1 : (current/350 < 0.5) ? 0.5 : current/350;
         ele1.css({'-webkit-transform' : 'scale('+ scale +')',
@@ -21,6 +31,8 @@ $(window).scroll(function (event) {
       current = current - 1100;
 
     }
+    
+
     ele2.css({marginLeft: total1 - current +'px'});
 });
 
